7a75a3b4324e7a139c2711e003fac3537d179d60
Luis Miguel Rodriguez <rodriguezmujica883@gmail.com>
2023-06-20 16:03:09 -0400

    ordenando las funciones

--- index.js
+++ index.js
@@ -10,33 +10,7 @@ let ingreseContraseña = prompt("Ingrese contraseña:");
 
 if (parseInt(ingreseContraseña) === cuenta.contraseña) {
   alert("Contraseña correcta");
-
-
-while (continuar){
-    mostrarMenu();
-    let opcion = parseInt(prompt("Ingrese una opción"));
-    
-    switch(opcion) {
-        case 1: 
-        consultarSaldo(cuenta.saldo);
-        break;
-        case 2:
-            let montoRetiro = parseInt(prompt("Ingrese monto a retirar"));
-            retirarDinero(montoRetiro, cuenta.saldo);
-            break;
-          case 3:
-            let montoDeposito = parseInt(prompt("Ingrese monto a depositar."));
-            depositarDinero(montoDeposito, cuenta.saldo);
-            break;
-        case 4:
-      continuar = salir()
-        break;
-        default:
-            alert("Opcion invalida.")
-            break;
-    }
-}
-
+  
 function mostrarMenu() {
 alert("Bienvenido Bancoder\n selecione una de las opciones \n 1- consulte su saldo \n 2- retire dinero \n 3- deposite dinero \n 4-salir ")
 }
@@ -84,6 +58,33 @@ function salir() {
       }
     } while (true);
   }
+
+while (continuar){
+    mostrarMenu();
+    let opcion = parseInt(prompt("Ingrese una opción"));
+    
+    switch(opcion) {
+        case 1: 
+        consultarSaldo(cuenta.saldo);
+        break;
+        case 2:
+            let montoRetiro = parseInt(prompt("Ingrese monto a retirar"));
+            retirarDinero(montoRetiro, cuenta.saldo);
+            break;
+          case 3:
+            let montoDeposito = parseInt(prompt("Ingrese monto a depositar."));
+            depositarDinero(montoDeposito, cuenta.saldo);
+            break;
+        case 4:
+      continuar = salir()
+        break;
+        default:
+            alert("Opcion invalida.")
+            break;
+    }
+}
+
+
 
   for (let i = 0; i < cuenta.historialMovimientos.length; i++) {
     let movimiento = cuenta.historialMovimientos[i];
